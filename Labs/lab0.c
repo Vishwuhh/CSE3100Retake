775027909540065ccd01c3485f71056d88448f1d
Vishwuhh <155852834+Vishwuhh@users.noreply.github.com>
2026-09-04 16:29:14 +0000

    Finished Lab1

--- Labs/lab0.c
+++ Labs/lab0.c
@@ -2,5 +2,15 @@
 
 int main() {
     printf("Hello, World!\n");
+    int n = 0;
+    int sum = 0; 
+    while(n < 200) {
+        if(n % 2 == 0) {
+            sum += n;
+            n++;
+        }
+        n++;
+    }
+    printf("%d\n", sum);
     return 0;
 }
