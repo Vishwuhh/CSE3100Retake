11651f1ff761c0d6c69e4f7d8bd13333404c4172
Vishwuhh <155852834+Vishwuhh@users.noreply.github.com>
2026-09-23 15:06:17 +0000

    HW 2 Completed

---
+++ oddSum.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int oddSumHelp(int count, int bound, int value)
+{
+	//fill in your code below
+	
+	// the number selected is the number of odd digits needed
+	if(count == 0) {
+		return value = 0;
+	}
+	// no more positive odd numbers exist or our sum is too far
+	if(bound <= 0 || value <= 0) {
+		return 0; // ends it
+	}
+	if(oddSumHelp(count -1, bound -2, value - bound)) {
+		printf("%d ", bound);
+
+		return 1;
+	}
+	return oddSumHelp(count, bound - 2, value);
+}
+
+//Do not change the code below
+void oddSum(int count, int bound, int value)
+{
+    	if(value <= 0 || count <= 0 || bound <= 0) return;
+    
+    	if(bound % 2 == 0) bound -= 1;
+
+    	if(!oddSumHelp(count, bound, value)) printf("No solutions.\n");
+	else printf("\n");
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc != 4) return -1;
+
+	int count = atoi(argv[1]);
+	int bound = atoi(argv[2]);
+	int value = atoi(argv[3]);
+
+	//oddSum(12,30,200);
+	//oddSum(10,20,100);
+	//oddSum(20,20,200);
+	oddSum(count, bound, value);
+	return 0;
+}
